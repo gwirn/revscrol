104bc93fb352f6bd1788af557ea15b18b33a089e
gwirn <71886945+gwirn@users.noreply.github.com>
2025-07-14 12:22:07 +0200

    executinh bash script

---
+++ revscrol.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mice=('MX Anywhere 2S' 'MX Master 3S')
+revscrolPath="$HOME/Code/revscrol"
+runtime="5M"
+
+externalMouseDetected=1
+
+endtime=$(date -u -v "+$runtime" +%s)
+while [[ $(date -u +%s) -le $endtime ]];do
+    for m in "${mice[@]}";do
+        if  ! system_profiler SPBluetoothDataType | grep -B1000 "$m" | grep -qi 'Not Connected' ;then
+            externalMouseDetected=0
+            break
+        fi
+    done
+    if [ "$externalMouseDetected" -eq 0 ];then
+        break
+    fi
+    sleep 5
+done
+
+if [ "$externalMouseDetected" -eq 1 ];then
+    exit 0
+fi
+if [ "$(defaults read -g com.apple.swipescrolldirection)" -eq 1 ];then
+    "$revscrolPath/revscrol" > "$revscrolPath/revscrol.log"&
+    echo "$!" > "$revscrolPath/revscrol.pid"
+fi
